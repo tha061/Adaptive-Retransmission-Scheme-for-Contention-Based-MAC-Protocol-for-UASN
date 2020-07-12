1fdff56e9524577b10088e913060b8ed93516371
Tham Nguyen <51147376+tha061@users.noreply.github.com>
2020-07-12 15:50:03 +1000

    Update uw-aloha-ar-init.tcl

--- ALOHA/uw-aloha-ar-init.tcl
+++ ALOHA/uw-aloha-ar-init.tcl
@@ -2,34 +2,9 @@
 # Copyright (c) 2012 Regents of the SIGNET lab, University of Padova.
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University of Padova (SIGNET lab) nor the 
-#    names of its contributors may be used to endorse or promote products 
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Saiful Azad
+
 
 Module/UW/ALOHA_AR set alohaar_debug_		0
-#Module/UW/ALOHA_AR set HDR_size_ 		0
 Module/UW/ALOHA_AR set ACK_size_  		10
 Module/UW/ALOHA_AR set max_tx_tries_		5
 Module/UW/ALOHA_AR set wait_constant_		0.1
@@ -46,7 +21,6 @@ Module/UW/ALOHA_AR set bit_rate_                  1400
 Module/UW/ALOHA_AR set interval_                  20
 Module/UW/ALOHA_AR set stop_time_                 500
 Module/UW/ALOHA_AR set boff_factor_               0.01 
-#Seting for CSMA-ARS
 Module/UW/ALOHA_AR set tham_debug_                0
 Module/UW/ALOHA_AR set HDR_size_ 	       10
 Module/UW/ALOHA_AR set number_of_flow_ar_       10   
